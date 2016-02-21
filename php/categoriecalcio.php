60073ee7f0cfa647c60772f5c8e79f6e10eca4e1
aleexpetty <alexfc.97@gmail.com>
2016-02-21 18:28:22 +0100

    Update categoriecalcio.php

--- php/categoriecalcio.php
+++ php/categoriecalcio.php
@@ -1,42 +1,40 @@
 <?php
 session_start();
-
-	$eta=$_GET["eta"];
+	$eta=0;
+	$result="";
+	$eta=$_GET["a"];
 	
 	//stabilisco connessione col server
-    $connection = mysql_connect("localhost", "root", "");
-	mysql_select_db("my_virtualcoaching",$connection);
+    $connection = mysqli_connect("localhost", "root", "");
+	mysqli_select_db($connection, "my_virtualcoaching");
     
 	//calcio a 5
+	$query="";
 	if($eta == "pulcini")
 	{
-		$query=mysql_query("SELECT IDSportCategoria FROM sportcat WHERE IDSportCategoria=1 "); 
-		$result=mysql_query($query);
+		$query="SELECT IDSportCategoria FROM sportcat WHERE IDSportCategoria=1 "; 	
 	}
 	else if($eta == "esordienti")
 	{
-		$query=mysql_query("SELECT IDSportCategoria FROM sportcat WHERE IDSportCategoria=2 "); 
-		$result=mysql_query($query);
+		$query="SELECT IDSportCategoria FROM sportcat WHERE IDSportCategoria=2 "; 
 	}
 	else if($eta == "giovanissimi")
 	{
-		$query=mysql_query("SELECT IDSportCategoria FROM sportcat WHERE IDSportCategoria=3 "); 
-		$result=mysql_query($query);
+		$query="SELECT IDSportCategoria FROM sportcat WHERE IDSportCategoria=3 "; 
 	}
 	else if($eta == "allievi")
 	{
-		$query=mysql_query("SELECT IDSportCategoria FROM sportcat WHERE IDSportCategoria=4 ");
-		$result=mysql_query($query);		
+		$query="SELECT IDSportCategoria FROM sportcat WHERE IDSportCategoria=4 "; 	
 	}
 	else if($eta == "juniores")
 	{
-		$query=mysql_query("SELECT IDSportCategoria FROM sportcat WHERE IDSportCategoria=5 "); 
-		$result=mysql_query($query);
+		$query="SELECT IDSportCategoria FROM sportcat WHERE IDSportCategoria=5 "; 
 	}
-		
-	$row=mysql_fetch_array($query);
+	
+	$result=$connection->query($query);
+	$row=$result->fetch_array();
     echo $row['IDSportCategoria'];
 	
-	mysql_close($connection);
+	$connection->close();
 
 ?>
