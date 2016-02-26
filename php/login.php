9847af62a5c65ef01c741f96b97b6aabf419849e
lotto8 <bottoli97@gmail.com>
2016-02-26 12:31:31 +0100

    Update login.php

--- php/login.php
+++ php/login.php
@@ -10,22 +10,25 @@ else
 $username=$_POST['email'];
 $password=$_POST['password'];
 // Stabilizzo la connessione col server passandogli come parametri il nome del server, l'id e la password
-$connection = mysql_connect("localhost", "root", "");
-mysql_select_db("my_virtualcoaching",$connection);
+$connection = mysqli_connect("localhost", "root", "");
+mysqli_select_db($connection,"my_virtualcoaching");
 // Tolgo i possibili / all'interno di username e password
 $username = stripslashes($username);
-$username = mysql_real_escape_string($username);
+$username = $connection->real_escape_string($username);
 // Seleziono il database
 // Query SQL che permette di controllare email e password con quelle all'interno del database
-$query = mysql_query("select * from user where password='".md5($password)."' AND email='$username'");
-$rows = mysql_num_rows($query);
+$query = $connection->query("select * from user where password='".md5($password)."' AND email='$username'");
+$rows = $query->num_rows;
+echo md5($password);
 if ($rows == 1) {
 $_SESSION['login_user']=$username; // Inizializzo la sessione
 header("location: ../index.php");  //richiamo il file index.php
+
 } else {
 $error = "Username or Password is invalid";
+echo $error;
 }
-mysql_close($connection); // Chiudo la connessione
+mysqli_close($connection); // Chiudo la connessione
 }
 
 ?>
