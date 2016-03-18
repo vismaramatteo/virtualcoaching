3663f6b8115f044e583e38b4e7cddadde66e39f1
aleexpetty <alexfc.97@gmail.com>
2016-03-18 17:01:40 +0100

    Jquery validation

--- index.php
+++ index.php
@@ -33,7 +33,182 @@ if(!isset($_SESSION['login_user']))
     <meta name="author" content="">
 	
  	<link rel="import" href="header.html">   
-
+	<link href="css/style.css" rel="stylesheet">
+	<link href="css/default.css" rel="stylesheet">
+	
+	<script>
+			function contattaci(val,val1,val2,val3,val4) 
+			{
+				var nome=$("#cname").val();
+				var mail=$("#cemail").val();
+				var tel=$("#cphone").val();
+				var oggetto=$("#csubject").val();
+				var commento=$("#ccomments").val();
+				
+				if(nome=="")
+				{
+					$("#cname").val("");
+					$("#cname").val("NOME MANCANTE!").animate({backgroundColor: "#c90000", color:"white"});
+					setTimeout(function(){
+					$("#cname").val("").animate({backgroundColor: "white"});
+								
+					},4000);
+				}
+				if(mail=="")
+				{
+					$("#cemail").val("");
+					$("#cemail").val("MAIL MANCANTE!").animate({backgroundColor: "#c90000", color:"white"});
+					setTimeout(function(){
+					$("#cemail").val("").animate({backgroundColor: "white"});
+								
+					},4000);
+				}
+				if(tel=="")
+				{
+					$("#cphone").val("");
+					$("#cphone").val("TELEFONO MANCANTE!").animate({backgroundColor: "#c90000", color:"white"});
+					setTimeout(function(){
+					$("#cphone").val("").animate({backgroundColor: "white"});
+								
+					},4000);
+				}
+				if(oggetto=="")
+				{
+					$("#csubject").val("");
+					$("#csubject").val("OGGETTO MANCANTE!").animate({backgroundColor: "#c90000", color:"white"});
+					setTimeout(function(){
+					$("#csubject").val("").animate({backgroundColor: "white"});
+								
+					},4000);
+				}
+				if(commento=="")
+				{
+					$("#ccomments").val("");
+					$("#ccomments").val("COMMENTO MANCANTE!").animate({backgroundColor: "#c90000", color:"white"});
+					setTimeout(function(){
+					$("#ccomments").val("").animate({backgroundColor: "white"});
+								
+					},4000);
+				}
+				
+				else if(nome!="" && mail!="" && tel!="" && oggetto!="" && commento!="")
+				{
+				$.post("php/contact.php",{"a":val,"b":val1,"c":val2,"d":val3,"e":val4},function(data){
+							$("#submit").html("");
+							$("#submit").html("MESSAGGIO INVIATO CORRETTAMENTE").animate({backgroundColor: "green"});
+							setTimeout(function(){
+								$("#submit").html("INVIA MESSAGGIO").animate({backgroundColor: "black", color:"white"});
+								
+							},4000);
+							
+					
+				});
+				}
+					
+			};
+			
+			
+			function registrati(val,val1,val2,val3,val4,val5) 
+			{
+				var nome=$("#rname").val();
+				var cognome=$("#cognome").val();
+				var telefono=$("#rphone").val();
+				var mail=$("#remail").val();
+				var password=$("#rpassword").val();
+				var sport=$("#rsport").val();
+				
+				if(nome=="")
+				{
+					$("#rname").val("");
+					$("#rname").val("NOME MANCANTE!").animate({backgroundColor: "#c90000", color:"white"});
+					setTimeout(function(){
+					$("#rname").val("").animate({backgroundColor: "white", color:"black"});
+								
+					},4000);
+				}
+				
+				if(cognome=="")
+				{	
+					$("#cognome").val("");
+					$("#cognome").val("COGNOME MANCANTE!").animate({backgroundColor: "#c90000", color:"white"});
+					setTimeout(function(){
+					$("#cognome").val("").animate({backgroundColor: "white", color:"black"});
+							
+					},4000);
+				}
+				if(telefono=="")
+				{
+					$("#rphone").val("");
+					$("#rphone").val("TELEFONO MANCANTE!").animate({backgroundColor: "#c90000", color:"white"});
+					setTimeout(function(){
+					$("#rphone").val("").animate({backgroundColor: "white", color:"black"});
+								
+					},4000);
+				}
+				if(mail=="")
+				{
+					$("#remail").val("");
+					$("#remail").val("E-MAIL MANCANTE!").animate({backgroundColor: "#c90000", color:"white"});
+					setTimeout(function(){
+					$("#remail").val("").animate({backgroundColor: "white", color:"black"});
+								
+					},4000);
+				}
+				if(password=="")
+				{
+					$("#rpassword").val("");
+					$("#rpassword").val("PASSWORD MANCANTE!").animate({backgroundColor: "#c90000", color:"white"});
+					setTimeout(function(){
+					$("#rpassword").val("").animate({backgroundColor: "white", color:"black"});
+								
+					},4000);
+				}
+				if(sport=="")
+				{
+					$("#rsport").val("");
+					$("#rsport").val("SPORT MANCANTE!").animate({backgroundColor: "#c90000", color:"white"});
+					setTimeout(function(){
+					$("#rsport").val("").animate({backgroundColor: "white", color:"black"});
+								
+					},4000);
+				}				
+				
+				
+				else if(nome!="" && cognome!="" && telefono!="" && mail!="" && password!="" && sport!="")
+				{
+				$.post("php/registrazione.php",{"a":val,"b":val1,"c":val2,"d":val3,"e":val4,"f":val5},function(data){
+					var LengthTel=$("#rphone").val().length;
+					alert(LengthTel);
+					if(data=="E-MAIL GI&Agrave; ESISTENTE")
+					{
+						$("#bregister").html("");
+						$("#bregister").html("E-MAIL GI&Agrave ESISTENTE");
+						
+						$("#remail").val(mail);
+						
+						$("#bregister").css({})
+						$(function () {
+							$("#bregister").animate({backgroundColor: "c90000"}, { duration: 200, queue: false });
+							$("#remail").animate({backgroundColor: "c90000", color:"white"},{ duration: 200, queue: false });
+						});
+						setTimeout(function(){
+							$("#remail").val(mail).animate({backgroundColor: "white", color:"black"});			
+							$("#bregister").html("REGISTRATI").animate({backgroundColor: "black", color:"white"});
+						},4000);
+					}
+					else
+					{
+						$("#bregister").html("");
+						$("#bregister").html("REGISTRAZIONE EFFETTUATA").animate({backgroundColor: "green"});
+						setTimeout(function(){
+						$("#bregister").html("REGISTRATI").animate({backgroundColor: "black", color:"white"});
+						},4000);
+					}
+				});
+				}
+			};
+	</script>
+	
     <title>VIRTUAL COACHING</title>
 
 
@@ -54,7 +229,7 @@ if(!isset($_SESSION['login_user']))
                 </button>
                 <a class="navbar-brand" href="index.php">
 					<div class="logo">
-						<img height="35" width="280" src="img/LOGO.png"> 
+						<img height="35" width="280" src="/virtualcoaching/img/LOGO.png"> 
 					</div>
                 </a>
             </div>
@@ -64,10 +239,9 @@ if(!isset($_SESSION['login_user']))
       <ul class="nav navbar-nav" style="background-color:white">
         <li class="active"><a href="#intro">Home</a></li>
         <li><a href="#sport">Sport</a></li>
-        <li><a href="organizzapartite.php">Organizza partita</a></li>
 		<li id="ultimo"><a href="#contact">Contattaci</a></li>
 		<li class="divider"></li>
-		<li class="dropdown">
+		<li id="dropdown" class="dropdown">
         			<?php 
                               if(!isset($_SESSION['login_user']))
                     echo ' <a href="#" class="dropdown-toggle" data-toggle="dropdown">Login <b class="caret"></b></a>
@@ -77,14 +251,14 @@ if(!isset($_SESSION['login_user']))
                            <div class="row">
                               <div class="col-md-12">
                               
-                                 <form class="form"  method="POST" action="./php/login.php" accept-charset="UTF-8" id="login-nav">
+                                 <form class="form"  method="POST" action="/virtualcoaching/php/login.php" accept-charset="UTF-8" id="login-nav">
                                     <div class="form-group">
                                        <label class="sr-only" for="email">E-Mail</label>
-                                       <input type="email" name="email" class="form-control" id="email" placeholder="Indirizzo E-Mail" required>
+                                       <input type="email" name="lemail" class="form-control" id="lemail" placeholder="Indirizzo E-Mail" required>
                                     </div>
                                     <div class="form-group">
                                        <label class="sr-only" for="password">Password</label>
-                                       <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
+                                       <input type="password" name="lpassword" class="form-control" id="lpassword" placeholder="Password" required>
                                     </div>
                                     
 									<a  href="index.php"
@@ -92,9 +266,9 @@ if(!isset($_SESSION['login_user']))
                                     PASSWORD DIMENTICATA? </a>
 									<br><br>
 									
-                                    <div class="form-group">
+                                    
                                        <button type="submit" class="btn btn-success btn-block">Accedi</button>
-                                    </div>
+                                    
                                     	
                                  </form> 
                                  <button href="#" onclick="$(&#39#example-popup&#39).show()" data-popup-target="#example-popup" class="btn btn-primary btn-block">Registrati</button>
@@ -112,7 +286,7 @@ if(!isset($_SESSION['login_user']))
 									<div class="col-md-12">
 										<div style="margin: 0 auto;">
 											<center>
-												<img class="fotorotonda" src="img/utente.png"> &nbsp;&nbsp;
+												<img class="fotorotonda" src="/virtualcoaching/img/utente.png"> &nbsp;&nbsp;
 												<strong> Ciao '.$_SESSION['login_user'].' </strong>
 											</center>
 										</div>
@@ -120,8 +294,8 @@ if(!isset($_SESSION['login_user']))
 									</div>
 																									
 									<div class="col-md-12">
-										<a style="float:left" id="aggiornadati" href="aggiornadati.php">Aggiorna dati</a>
-										<a style="float:right" id="logout" href="/php/logout.php">Logout</a>
+										<a style="float:left" id="aggiornadati" href="/virtualcoaching/aggiornadati.php">Aggiorna dati</a>
+										<a style="float:right" id="logout" href="/virtualcoaching//php/logout.php">Logout</a>
 									</div>
 								  
 								</div>
@@ -189,14 +363,14 @@ if(!isset($_SESSION['login_user']))
 
         
         
-			<a href="/calcio.php">
+			<a href="/virtualcoaching/calcio.php">
 				<div class="col-xs-6 col-sm-6 col-md-6">
 					<div class="wow bounceInDown" data-wow-delay="0.2s">
 					<div class="team boxed-grey">
 						<div class="inner">
 							<h5>CALCIO</h5>
                             <center>
-							<div class="avatar"><img src="img/team/pallacalcio.jpg" alt="" class="img-responsive img-circle" /></div>
+							<div class="avatar"><img src="/virtualcoaching/img/team/pallacalcio.jpg" alt="" class="img-responsive img-circle" /></div>
 							</center>
                         </div>
 					</div>
@@ -204,14 +378,14 @@ if(!isset($_SESSION['login_user']))
 				</div>
 			</a>
 			
-			<a href="/basket.php">
+			<a href="/virtualcoaching/basket.php">
 				<div class="col-xs-6 col-sm-6 col-md-6">
 					<div class="wow bounceInDown" data-wow-delay="0.2s">
 					<div class="team boxed-grey">
 						<div class="inner">
 							<h5>BASKET</h5>
                             <center>
-							<div class="avatar"><img src="img/team/pallabasket.jpg" alt="" class="img-responsive img-circle" /></div>
+							<div class="avatar"><img src="/virtualcoaching/img/team/pallabasket.jpg" alt="" class="img-responsive img-circle" /></div>
 							</center>
                         </div>
 					</div>
@@ -254,32 +428,32 @@ if(!isset($_SESSION['login_user']))
 					<div class="container">
 						<div id="map-canvas"></div>
 						<div class="row">				
-							<form method="post" action="../php/contact.php" name="contactform" id="contactform">
+							<div name="contactform" id="contactform">
 									<div class="col-md-6">
 										<fieldset>
-											<input name="name" type="text" id="name" size="30" placeholder="Nome e Cognome">
+											<input name="cname" type="text" id="cname" size="30" placeholder="Nome e Cognome">
 											<br>
-											<input name="email" type="text" id="email" size="30" placeholder="Email">
+											<input name="cemail" type="text" id="cemail" size="30" placeholder="Email">
 											<br>
-											<input name="phone" type="text" id="phone" size="30" placeholder="Telefono">
+											<input name="cphone" type="text" id="cphone" size="30" placeholder="Telefono">
 											<br>
-											<input name="subject" type="text" id="subject" size="30" placeholder="Oggetto">
+											<input name="csubject" type="text" id="csubject" size="30" placeholder="Oggetto">
 											<br>
 										</fieldset>
 									</div>
 									
 									<div class="col-md-6">
 									  <fieldset>
-										<textarea name="comments" cols="40" rows="20" id="comments" placeholder="Messaggio"></textarea>
+										<textarea name="ccomments" cols="40" rows="20" id="ccomments" placeholder="Messaggio"></textarea>
 									  </fieldset>
 									</div>
 									
-									<div class="col-md-12">
+									<div class="col-md-12" >
 									  <fieldset>
-										<button type="submit" class="btn btn-lg" id="submit" value="Submit">Invia Messaggio</button>
+										<button type="submit" class="btn btn-lg" onclick="contattaci($('#cname').val(),$('#cemail').val(),$('#cphone').val(),$('#csubject').val(),$('#ccomments').val());" id="submit" value="Submit">Invia Messaggio</button>
 									  </fieldset>
 									</div>
-							</form>
+							</div>
 						</div>
 					</div>
 				</section>
@@ -293,7 +467,7 @@ if(!isset($_SESSION['login_user']))
 				<div class="col-md-3">
 					<div class="wow bounceInUp" data-wow-delay="0.2s">
 						<center>
-							<img class="img-circle img-responsive" height="180" width="180" src="img/workers/bobo.jpg">
+							<img class="img-circle img-responsive" height="180" width="180" src="/virtualcoaching/img/workers/bobo.jpg">
 							<br>
 							<h4>Bottoli <br>Luca</h4>
 						</center>
@@ -303,7 +477,7 @@ if(!isset($_SESSION['login_user']))
 				<div class="col-md-3">
 					<div class="wow bounceInUp" data-wow-delay="0.2s">
 						<center>
-							<img class="img-circle img-responsive" height="180" width="180" src="img/workers/garra.jpg">
+							<img class="img-circle img-responsive" height="180" width="180" src="/virtualcoaching/img/workers/garra.jpg">
 							<br>
 							<h4>Garraffa <br>Alexandro</h4>
 						</center>
@@ -313,7 +487,7 @@ if(!isset($_SESSION['login_user']))
 				<div class="col-md-3">
 					<div class="wow bounceInUp" data-wow-delay="0.2s">
 						<center>
-							<img class="img-circle img-responsive" height="180" width="180" src="img/workers/petty.jpg">
+							<img class="img-circle img-responsive" height="180" width="180" src="/virtualcoaching/img/workers/petty.jpg">
 							<br>
 							<h4>Pettinà <br>Alessandro</h4>
 						</center>
@@ -323,7 +497,7 @@ if(!isset($_SESSION['login_user']))
 				<div class="col-md-3">
 					<div class="wow bounceInUp" data-wow-delay="0.2s">
 						<center>
-							<img class="img-circle img-responsive" height="180" width="180" src="img/workers/visma.jpg">
+							<img class="img-circle img-responsive" height="180" width="180" src="/virtualcoaching/img/workers/visma.jpg">
 							<br>
 							<h4>Vismara <br>Matteo</h4>
 						</center>
@@ -331,41 +505,36 @@ if(!isset($_SESSION['login_user']))
 				</div>
                 
 
-			<form class="form" method="POST" action="./php/registrazione.php">
-				        <div class="row">
-			<div id="example-popup" class="popup" style="display:none;"  >
+			<div class="row">
+			<div id="example-popup" class="popup" style="display:none;">
     			<div class="popup-body" style="height:510px">	<span onclick="$(&#39#example-popup&#39).hide()" class="popup-exit"></span>
         			<div class="popup-content" >
             			<h2 class="popup-title">REGISTRATI</h2>
                           <div class="register" id="register" >
                               <div class="col-md-12">
 								<fieldset>
-                                      <input name="name" type="text" id="name" size="30" style="border: 1px solid #323232" placeholder="INSERIRE Nome "></input>
+                                      <input name="rname" type="text" id="rname" size="30" style="border: 1px solid #323232" placeholder="INSERIRE Nome"></input>
                                       <br>
-                                      <input name="surname" type="text" id="surname" size="30" style="border: 1px solid #323232" placeholder="INSERIRE Cognome"></input>
+                                      <input name="cognome" type="text" id="cognome" size="30" style="border: 1px solid #323232" placeholder="INSERIRE Cognome"></input>
                                       <br>
-                                      <input name="phone" type="text" id="phone" size="30" style="border: 1px solid #323232" placeholder="INSERIRE Telefono"></input>
+                                      <input name="rphone" type="number" id="rphone" size="30" style="border: 1px solid #323232" placeholder="INSERIRE Telefono"></input>
                                       <br>
-                                      <input name="email" type="text" id="email" size="30" style="border: 1px solid #323232" placeholder="INSERIRE Email"></input>
+                                      <input name="remail" type="email" id="remail" size="30" style="border: 1px solid #323232" placeholder="INSERIRE Email" required></input>
                                       <br>
-                                      <input name="password" type="password" id="password" size=30 style="border: 1px solid #323232" placeholder="INSERIRE Password" required></input>
+                                      <input name="rpassword" type="password" id="rpassword" size=30 style="border: 1px solid #323232" placeholder="INSERIRE Password"></input>
                                       <br>
                                       <div class="sportpraticato">
 										SPORT PRATICATO?
-                               				<select style="height:30px; width:200px" name="sport" id="sport">
+                               				<select style="height:30px; width:200px" name="rsport" id="rsport">
 												<option value="calcio">CALCIO</option>
 												<option value="basket">BASKET</option>
 											</select>
 										</div>
                                       <br>
 									  <!--<input name="img_utente" type="file" id="img_utente" style="border: 1px solid #323232" name="file_inviato"><br></input>-->
-                                      
 
-										
-                                       
-                                        
 										<div class="col-md-12">
-											<button type="submit" class="btn btn-lg" id="register">REGISTRATI</button>
+											<button type="submit" class="btn btn-lg" id="bregister" onclick="registrati($('#rname').val(),$('#cognome').val(),$('#rphone').val(),$('#remail').val(),$('#rpassword').val(),$('#rsport').val());">REGISTRATI</button>
 										</div>	
                                         <br>
                                   </fieldset>
@@ -379,7 +548,6 @@ if(!isset($_SESSION['login_user']))
 				
                 
                 </div>
-                </form>
 			</div>
            
 		</div>
@@ -398,7 +566,7 @@ if(!isset($_SESSION['login_user']))
 						</a>	
 					</div>
 					</div>
-					<center><img class="img-circle img-responsive" height="100" width="300" src="img/LOGO LAMA.png"></center>
+					<center><img class="img-circle img-responsive" height="100" width="300" src="/virtualcoaching/img/LOGO LAMA.png"></center>
 				</div>
 			</div>	
 		</div>
